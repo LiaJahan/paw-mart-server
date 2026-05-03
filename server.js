d46fe067d4230866b9503a84cf255c35a0685f49
LiaJahan <jesmoonlia@gmail.com>
2026-05-03 14:34:34 +0600

    fixing another field

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import Listing from "./models/Listing.js";
 import listingsRouter from "./routes/listings.js";
 import ordersRouter from "./routes/orders.js";
 
@@ -12,17 +12,14 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/orders", ordersRouter);
-
-mongoose.connect(
-  "mongodb+srv://pawMartUser:ShEcFYfAfM8rZPdl@cluster0.ph8mefq.mongodb.net/paw-mart?retryWrites=true&w=majority"
-)
-
-.then(() => console.log("MongoDB connected perfectly"))
-.catch(err => console.log(err));
-
-
 app.use("/listings", listingsRouter);
 
-const PORT = 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
+mongoose.connect(process.env.DATABASE_URL)
+  .then(() => console.log("MongoDB connected perfectly"))
+  .catch(err => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
